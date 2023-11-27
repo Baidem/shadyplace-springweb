31991db4251b7d6010fadd24b497a5a80599bf9e
vincent <baidem@gmail.com>
2023-11-27 23:00:45 +0100

    DateOrderConstraintValidator : minor update

--- src/main/java/com/shadyplace/springweb/constraints/DateOrderConstraintValidator.java
+++ src/main/java/com/shadyplace/springweb/constraints/DateOrderConstraintValidator.java
@@ -14,11 +14,8 @@ public class DateOrderConstraintValidator implements ConstraintValidator<DateOrd
     @Override
     public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
         BookingForm bookingForm = (BookingForm) o;
-        boolean isValid = false;
 
         if (bookingForm.getDateStart() != null && bookingForm.getDateEnd() != null){
-            boolean b = bookingForm.getDateStart().getTime() <= bookingForm.getDateEnd().getTime();
-            System.out.println(b);
             return bookingForm.getDateStart().getTime() <= bookingForm.getDateEnd().getTime();
         } else {
             return false;
