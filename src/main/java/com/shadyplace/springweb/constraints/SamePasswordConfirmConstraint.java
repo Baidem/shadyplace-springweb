7ad29cb9e76eabf234ca89eed24d4657efd4e7af
vincent <baidem@gmail.com>
2023-11-27 23:07:51 +0100

    SamePasswordConfirmConstraintValidator + SamePasswordConfirmConstraint

---
+++ src/main/java/com/shadyplace/springweb/constraints/SamePasswordConfirmConstraint.java
@@ -0,0 +1,23 @@
+package com.shadyplace.springweb.constraints;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = SamePasswordConfirmConstraintValidator.class)
+@Target({ElementType.TYPE}) // two fields!
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SamePasswordConfirmConstraint {
+
+    String message() default "The new password must be the same as the confirmation password.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {}; // request content
+
+    String fieldNewPassword();
+    String fieldConfirmPassword();
+
+}
