481b86dd693ec8cf1dacfce281d58f582ce75359
vincent <baidem@gmail.com>
2023-12-05 11:45:16 +0100

    FamilyLinkRepository.java : findAll()

--- src/main/java/com/shadyplace/springweb/repository/userAuth/FamilyLinkRepository.java
+++ src/main/java/com/shadyplace/springweb/repository/userAuth/FamilyLinkRepository.java
@@ -5,7 +5,11 @@ import com.shadyplace.springweb.models.enums.FamilyLinkLabel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FamilyLinkRepository extends CrudRepository<FamilyLink, Long> {
     FamilyLink findFirstByLabel(FamilyLinkLabel familyLinkLabel);
+    @Override
+    List<FamilyLink> findAll();
 }
